749a6aa57385f9c868b83db0e07a4e7ecc1114f5
CodeLord0 <tristan.emeana@gmail.com>
2024-07-21 21:46:36 +0100

    more on strings1

--- MyConsoleApp/Strings.cs
+++ MyConsoleApp/Strings.cs
@@ -10,11 +10,31 @@ namespace MyConsoleApp
     {
         static void Main(string[] args)
         {
+            string phrase = "say hi";
             Console.WriteLine("Heloo World"); //this is the command to print something
-                                          
-                                          // 
             Console.WriteLine("Hello\nWorld!");// \n prints the text after it in a new line
+
+            // string methods and properties
+            // properties are used to acess data about a datatype or string etc e.g Length. while meathods give instructions to be performed
+            //on a data type and are followed by a bracket e.g .ToUpper()
+            Console.WriteLine(phrase.Length);// this is a property
+            Console.WriteLine(phrase.ToUpper());//this is a method
+            Console.WriteLine(phrase.Contains("hello"));//some methods can takeon argumets
+
+            //indexing
+            //individual elements of a string can be acessed from indidaual indexes
+            Console.WriteLine(phrase[0]);//
+
+
+
+
+
+
             Console.ReadLine();
+
+
+
+
         }
     }
 }
