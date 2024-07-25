e2fae1afb48e53b9d0b86897119e1ff92aa4d512
CodeLord0 <tristan.emeana@gmail.com>
2024-07-25 23:26:44 +0100

    numbers added

---
+++ MyConsoleApp/numbers.cs
@@ -0,0 +1,36 @@
+﻿/*using System;
+
+namespace MyConsoleAp
+{
+    class number
+    {
+        static void Main(string[] args)
+        {
+            //integer + decimal= decimal
+            //inter + integer = integer
+            Console.WriteLine(5 / 2);//this returns an intrger
+
+            int num = 45;
+            num++;// increment a value by one just like num+=1 in python
+            num--;//decrement a value by one just like num-=1 in python
+            Console.WriteLine(num);
+
+            //math method
+            Console.WriteLine(Math.Pow(2,3));
+            Console.WriteLine(Math.Abs(-40));
+            Console.WriteLine(Math.Sqrt(100));
+            Console.WriteLine(Math.Max(10, 8));//finds the max of two values
+            Console.WriteLine(Math.Round(10.45));
+
+          
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}*/
