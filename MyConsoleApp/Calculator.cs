beee42e0530e8290804596e5fa8216af1539721d
CodeLord0 <tristan.emeana@gmail.com>
2024-08-01 18:39:01 +0100

    added calculator

---
+++ MyConsoleApp/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter a number: ");// promt to enter a number
+
+            double num = Convert.ToDouble(Console.ReadLine());// convert string to a double
+
+            Console.Write("enter another number: ");// promt for second number
+
+            double num2 = Convert.ToDouble(Console.ReadLine());// second string to double
+
+            Console.WriteLine(num+num2);// adds nums together
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+
+    }
+}
