c30391c0e181c31c25b8b9d5c8a3093b555a94e9
CodeLord0 <tristan.emeana@gmail.com>
2024-08-01 17:46:13 +0100

    userinput added

---
+++ MyConsoleApp/Userinput.cs
@@ -0,0 +1,42 @@
+﻿/*using System;
+
+namespace MyConsoleApp
+{
+    class Userinput
+    {
+        static void Main(string []args)
+        {
+
+
+            //console.write() prints out text and remains on the same line
+            
+            Console.Write("enter your name: ");//this promts a name
+            
+            string name = Console.ReadLine();//this listens to the promt given 
+
+
+            Console.WriteLine("hello "+ name );//this says the users name
+
+
+            Console.WriteLine(name + " how old are you ");//prompts for an age
+
+            string age = Console.ReadLine();//stores the age given
+
+        
+            Console.WriteLine(name + " you are " + age);// prints name and age
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+
+}*/
