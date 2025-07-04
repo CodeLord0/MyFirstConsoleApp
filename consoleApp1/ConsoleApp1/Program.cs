d5a3d97ef1af1898d6d8868622544ac4ce82bbc4
CodeLord0 <tristan.emeana@gmail.com>
2025-07-04 18:59:52 +0100

    ConsoleApp1 added
    a challenge on a book I am using to learn to c#

---
+++ consoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("Enter target column: ");
+            string targetColumn = Console.ReadLine();
+
+            Console.Write("Enter target row: ");
+            string targetRow = Console.ReadLine();
+            Console.Clear();
+
+            int squadtop_x = Convert.ToInt32(targetColumn);
+            int squadtop_y = Convert.ToInt32(targetRow) + 1;
+            
+            int squadleft_x = Convert.ToInt32( targetColumn) - 1;
+            int squadleft_y = Convert.ToInt32(targetRow);
+
+            int squadright_x = Convert.ToInt32(targetColumn) + 1;
+            int squadright_y = Convert.ToInt32(targetRow);
+
+            int squadbottom_x = Convert.ToInt32(targetColumn);
+            int squadbottom_y = Convert.ToInt32(targetRow) -1;
+
+            Console.WriteLine($"Target row: {targetColumn}");
+            Console.WriteLine($"Target column: {targetRow}");
+
+            
+            Console.WriteLine($"deploy to:");
+            Console.WriteLine($"{squadtop_x},{squadtop_y}");
+            Console.WriteLine($"{squadbottom_x},{squadbottom_y}");
+            Console.WriteLine($"{squadright_x},{squadright_y}");
+            Console.WriteLine($"{squadleft_x},{squadleft_y}");
+            
+            Console.Beep(400, 1000);
+            Console.ReadLine();
+
+
+
+
+           
+
+
+
+
+        }
+
+    }
+}
