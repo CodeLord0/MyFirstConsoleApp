e2c7c135ef6fdd1ca97b8f86ea49ad398cb5a171
CodeLord0 <tristan.emeana@gmail.com>
2025-07-22 23:39:57 +0100

    memory management excersie

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+//int num = 12;
+
+
+
+
+
+
+int Instead(int number)
+{
+    
+    
+    if(number == 0) return 1;
+
+    else
+    {
+        Console.WriteLine(number);
+        return Instead(number -1 );
+    }
+    
+    
+}
+
+Instead(10);
---
+++ MemoryManagement/MemoryManagement/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Metrics;
+using System.Numerics;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+int ManticoreHealth = 10;
+int CityHealth = 15;
+int roundNumber = 1;
+Console.WriteLine("Player 1, how far away from the city do you want to station the Manticore? ");
+string playerOneInput = Console.ReadLine();
+int ManticoreDistance = Convert.ToInt32(playerOneInput);
+Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Player 2, it is your turn.");
+string PlayerTwoInput = Console.ReadLine();
+int playerTwoGuess = Convert.ToInt32(PlayerTwoInput);
+
+while (ManticoreHealth > 0 & CityHealth > 0)
+{
+    Compute();
+    roundNumber++;
+}
+Console.WriteLine("you Win");
+
+
+void Compute()
+{
+    if (roundNumber % 3 == 0 & roundNumber % 5 == 0)
+    {
+        CityHealth--;
+        ManticoreHealth -= 10;
+        Console.WriteLine($"STATUS: Round: {roundNumber}  City: {CityHealth} / 15  Manticore: {ManticoreHealth} / 10");
+        Console.WriteLine("That was a direct hit");
+        Console.WriteLine("The cannon is expected to deal 10 damage this round");
+        Console.Write("Entered desired range: ");
+        PlayerTwoInput = Console.ReadLine();
+        playerTwoGuess = Convert.ToInt32(PlayerTwoInput);
+    }
+
+    else if (roundNumber % 3 != 0 & roundNumber % 5 != 0 & playerTwoGuess == ManticoreDistance)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        CityHealth--;
+        ManticoreHealth--;
+        Console.WriteLine($"STATUS: Round: {roundNumber}  City: {CityHealth} / 15  Manticore: {ManticoreHealth} / 10");
+        Console.WriteLine("The cannon is expected to deal 1 damage this round");
+        Console.WriteLine("Entered desired range: ");
+        Console.WriteLine("That was a direct hit");
+        PlayerTwoInput = Console.ReadLine();
+        playerTwoGuess = Convert.ToInt32(PlayerTwoInput);
+    }
+
+
+
+    else if (playerTwoGuess > ManticoreDistance & roundNumber % 3 == 0 || roundNumber % 5 == 0)
+    {
+        CityHealth--;
+        ManticoreHealth -= 3;
+        Console.WriteLine($"STATUS: Round: {roundNumber}  City: {CityHealth} / 15  Manticore: {ManticoreHealth} / 10");
+        Console.WriteLine("The cannon is expected to deal 3 damage this round");
+        Console.WriteLine("Entered desired range: ");
+        Console.WriteLine("That round overshot the target");
+        PlayerTwoInput = Console.ReadLine();
+        playerTwoGuess = Convert.ToInt32(PlayerTwoInput);
+    }
+    else if (playerTwoGuess < ManticoreDistance & roundNumber % 3 == 0 || roundNumber % 5 == 0)
+    {
+        CityHealth --;
+        ManticoreHealth -= 3;
+        Console.WriteLine($"STATUS: Round: {roundNumber}  City: {CityHealth} / 15  Manticore: {ManticoreHealth} / 10");
+        Console.WriteLine("The cannon is expected to deal 3 damage this round");
+        Console.WriteLine("Entered desired range: ");
+        Console.WriteLine("That round fell short of the target");
+        PlayerTwoInput = Console.ReadLine();
+        playerTwoGuess = Convert.ToInt32(PlayerTwoInput);
+    }
+}
